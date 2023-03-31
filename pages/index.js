960f070f6bad5bea4a09c8daa5889460a41583a9
Daniil Goncharov <goncharov10d@gmail.com>
2023-03-31 16:31:19 +0500

    resore v1

---
+++ pages/index.js
@@ -0,0 +1,166 @@
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemoffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemoffset - animItemPoint) && pageYOffset < (animItemoffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      } else {
+        if (!animItem.classList.contains('_anim-no-hide')) {
+          animItem.classList.remove('_active');
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+}
+setTimeout(() => {
+  animOnScroll();
+}, 300);
+const slidesContainer = document.querySelector('.my-projects__container');
+
+
+const langBtns = document.querySelectorAll('.lang-menu__btn');
+const allLang = ['en', 'ru'];
+const enBtn = document.querySelector('.enBtn');
+const ruBtn = document.querySelector('.ruBtn');
+
+function setLanguageIndicator(lang) {
+  if (lang === 'en') {
+    enBtn.classList.add('lang-menu__btn_select');
+    ruBtn.classList.remove('lang-menu__btn_select');
+  } else {
+    ruBtn.classList.add('lang-menu__btn_select');
+    enBtn.classList.remove('lang-menu__btn_select');
+  }
+}
+
+langBtns.forEach((item) => {
+  item.addEventListener('click', changeUrlLanguage);
+});
+
+function changeUrlLanguage(evt) {
+  let lang = evt.target.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+};
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  setLanguageIndicator(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  };
+  for (let key in langArr) {
+    let elem = document.querySelector('.lng-' + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
+  };
+};
+
+changeLanguage();
+
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const container = document.querySelector('.my-projects__container')
+const slidesCount = mainSlide.querySelectorAll('div').length;
+const body = document.body;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+  changeSlide('down');
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'ArrowUp') {
+    changeSlide('up');
+  } else if (evt.key === 'ArrowDown') {
+    changeSlide('down');
+  }
+})
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex === slidesCount)
+    {
+      activeSlideIndex = 0
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0)
+    {
+      activeSlideIndex = slidesCount - 1
+    }
+  }
+
+  const height = container.clientHeight
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
+
+container.addEventListener('touchstart', handleTouchStart, false);
+      slidesContainer.addEventListener('touchmove', handleTouchMove, false);
+      slidesContainer.addEventListener('touchend', () => {
+        body.classList.remove('disable-scroll');
+      });
+
+      let x1 = null;
+      let y1 = null;
+
+      function handleTouchStart(evt) {
+        const firstTouch = evt.touches[0];
+        x1 = firstTouch.clientX;
+        y1 = firstTouch.clientY;
+        body.classList.add('disable-scroll');
+      }
+
+      function handleTouchMove(evt) {
+        if (!x1 || !y1) {
+          return false;
+        }
+        let x2 = evt.touches[0].clientX;
+        let y2 = evt.touches[0].clientY;
+        let xDiff = x2 - x1;
+        let yDiff = y2 - y1;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {
+          if (xDiff > 0);
+        } else {
+          if (yDiff > 0) {
+            changeSlide('down');
+          } else {
+            changeSlide('up');
+          }
+        }
+        x1 = null;
+        y2 = null;
+      }
---
+++ utils/constants.js
@@ -0,0 +1,102 @@
+const langArr = {
+  "menu-main" : {
+    "ru": "Главное",
+    "en": "Main"
+  },
+  "menu-about" : {
+    "ru": "Кто я",
+    "en": "Who I am"
+  },
+  "menu-projects" : {
+    "ru": "Мои проекты",
+    "en": "My projects"
+  },
+  "menu-contact" : {
+    "ru": "Контакты",
+    "en": "Contact"
+  },
+  "name" : {
+    "ru": "Даниил Гончаров",
+    "en": "Daniil Goncharov"
+  },
+  "job" : {
+    "ru": "Веб-разработчик",
+    "en": "Web-developer"
+  },
+  "about" : {
+    "ru": "Обо мне",
+    "en": "About myself"
+  },
+  "short-description" : {
+    "ru": "Привет! Я веб-разработчик, занимаюсь версткой адаптивных сайтов. Хорошо знаю Java Script и React.",
+    "en": "Hi! I am a web developer, I make responsive websites. I am good at Java Script and React."
+  },
+  "long-description-one" : {
+    "ru": "Родился в Москве. В 2012 году закончил Российский Государственный Гуманитарный Университет по специальности социальная психология. 10 лет работал в сфере развития и обучения персонала. Разрабатывал и проводил учебные курсы, тренинги, дистанционные он-лайн курсы. В 2021 году перешел работать в IT стартап где занимался «Управлением знаниями», а именно писал документацию для разработчиков по софту,который разрабатывала компания. Организовал учебный портал для хранения этой информации и для этого начал изучать html верстку.",
+    "en": "I was born in Moscow. In 2012 I graduated from the Russian State University for the Humanities with a degree in social psychology. 10 years of experience in staff development and training. Developed and conducted training courses, trainings, distance online courses. In 2021, I moved to work at an IT startup where I was engaged in Knowledge Management, specifically, I wrote software documentation of methodological and training materials for clients using the software created by the company. I organized an educational portal for storing this information and for this I began to study html layout."
+  },
+  "long-description-two" : {
+    "ru": "В 2022 году решил, что хочу полностью посветить себя разработке сайтов и прошел учебный курс Яндекс Практику, чтобы стать сертефицированным Веб-разработчиком.",
+    "en": "In 2022, I decided expanding my career into web development. Finishing the Software Engineer course at Yandex Practicum to become a certified Web Developer."
+  },
+  "skills" : {
+    "ru": "Мои навыки",
+    "en": "My skills"
+  },
+  "projects" : {
+    "ru": "Мои проекты",
+    "en": "My projects"
+  },
+  "how-to-learn" : {
+    "ru": "Научиться учиться",
+    "en": "How to learn"
+  },
+  "russian-travel" : {
+    "ru": "Путишествие по России",
+    "en": "Russian travel"
+  },
+  "mesto" : {
+    "ru": "проект Место",
+    "en": "project Mesto"
+  },
+  "aim-training" : {
+    "ru": "игра Тренировка меткости",
+    "en": "Aim-Training game"
+  },
+  "vertical-slider" : {
+    "ru": "сайт с вертикальным слайдером изображений",
+    "en": "website with Vertical Slider"
+  },
+  "contact" : {
+    "ru": "Контакты",
+    "en": "Contact"
+  },
+  "linkedin" : {
+    "ru": "Моя страница в LinkedIn",
+    "en": "My page in LinkedIn"
+  },
+  "telegram" : {
+    "ru": "Мой Telegram",
+    "en": "My Telegram"
+  },
+  "view-website-1" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-2" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-3" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-4" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-5" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+};
